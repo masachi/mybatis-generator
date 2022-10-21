914581aeb096f889a7091106b1a973fb61f6e43b
masachi <zhangjb42@gmail.com>
2022-10-21 14:56:09 +0800

    first commit

---
+++ src/main/java/org/mybatis/generator/codegen/mybatis3/xmlmapper/elements/InsertOnDuplicateKeyUpdateElementGenerator.java
@@ -0,0 +1,127 @@
+/*
+ *    Copyright 2006-2022 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.generator.codegen.mybatis3.xmlmapper.elements;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.dom.OutputUtilities;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.codegen.mybatis3.ListUtilities;
+import org.mybatis.generator.codegen.mybatis3.MyBatis3FormattingUtilities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertOnDuplicateKeyUpdateElementGenerator extends AbstractXmlElementGenerator {
+
+    private final boolean isSimple;
+
+    public InsertOnDuplicateKeyUpdateElementGenerator(boolean isSimple) {
+        super();
+        this.isSimple = isSimple;
+    }
+
+    @Override
+    public void addElements(XmlElement parentElement) {
+        FullyQualifiedJavaType parameterType;
+        if (isSimple) {
+            parameterType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
+        } else {
+            parameterType = introspectedTable.getRules().calculateAllFieldsClass();
+        }
+
+        XmlElement answer = buildInitialInsert(introspectedTable.getInsertStatementId(), parameterType);
+
+        StringBuilder insertClause = new StringBuilder();
+
+        insertClause.append("insert into "); //$NON-NLS-1$
+        insertClause.append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
+        insertClause.append(" ("); //$NON-NLS-1$
+
+        StringBuilder valuesClause = new StringBuilder();
+        valuesClause.append("values ("); //$NON-NLS-1$
+
+        List<String> valuesClauses = new ArrayList<>();
+        List<IntrospectedColumn> columns =
+                ListUtilities.removeIdentityAndGeneratedAlwaysColumns(introspectedTable.getAllColumns());
+        for (int i = 0; i < columns.size(); i++) {
+            IntrospectedColumn introspectedColumn = columns.get(i);
+
+            insertClause.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
+            valuesClause.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn));
+            if (i + 1 < columns.size()) {
+                insertClause.append(", "); //$NON-NLS-1$
+                valuesClause.append(", "); //$NON-NLS-1$
+            }
+
+            if (valuesClause.length() > 80) {
+                answer.addElement(new TextElement(insertClause.toString()));
+                insertClause.setLength(0);
+                OutputUtilities.xmlIndent(insertClause, 1);
+
+                valuesClauses.add(valuesClause.toString());
+                valuesClause.setLength(0);
+                OutputUtilities.xmlIndent(valuesClause, 1);
+            }
+        }
+
+        insertClause.append(')');
+        answer.addElement(new TextElement(insertClause.toString()));
+
+        valuesClause.append(')');
+        valuesClauses.add(valuesClause.toString());
+
+        for (String clause : valuesClauses) {
+            answer.addElement(new TextElement(clause));
+        }
+
+        // on duplicate key update
+        StringBuilder duplicateClause = new StringBuilder();
+        duplicateClause.append("on duplicate key update");
+
+        List<String> duplicateClauses = new ArrayList<>();
+
+        for (int i = 0; i < columns.size(); i++) {
+            IntrospectedColumn introspectedColumn = columns.get(i);
+
+            duplicateClause.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
+            duplicateClause.append("=");
+            duplicateClause.append("values(");
+            duplicateClause.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
+            duplicateClause.append(")");
+            if (i + 1 < columns.size()) {
+                duplicateClause.append(", "); //$NON-NLS-1$
+            }
+
+            if (duplicateClause.length() > 80) {
+                duplicateClauses.add(duplicateClause.toString());
+                duplicateClause.setLength(0);
+                OutputUtilities.xmlIndent(duplicateClause, 1);
+            }
+        }
+        duplicateClauses.add(valuesClause.toString());
+
+        for (String clause : duplicateClauses) {
+            answer.addElement(new TextElement(clause));
+        }
+
+
+        if (context.getPlugins().sqlMapInsertElementGenerated(answer, introspectedTable)) {
+            parentElement.addElement(answer);
+        }
+    }
+}
